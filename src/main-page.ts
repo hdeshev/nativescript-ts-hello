e297b93631c6d2193e1005dfb0c937e09c50ab71
Neli <neli.chakarova@yahoo.com>
2015-11-02 11:18:09 +0200

    Working with 1.4 modules

--- src/main-page.ts
+++ src/main-page.ts
@@ -1,8 +1,8 @@
 import {HelloWorldModel} from "./main-view-model";
-
+import {Page} from "ui/page";
 var viewModel = new HelloWorldModel();
 
 export function pageLoaded(args) {
-    var page = args.object;
+    var page = <Page>args.object;
     page.bindingContext = viewModel;
 }
